9f7a934bbed3c8ed23c215d9a9f807c35a82a1eb
unknown <denislav9711@gmail.com>
2021-12-21 22:50:41 +0200

    add layout, styles and some functionality for total cart

--- scripts/shopping.js
+++ scripts/shopping.js
@@ -2,17 +2,31 @@ const cart = document.getElementsByClassName("cart");
 const totalCart = document.getElementById("cart-total");
 const clearCartElement = document.getElementById("clear-cart");
 
+const productImages = document.getElementsByClassName("product-wrapper");
+const productTitles = document.getElementsByClassName("title-specs");
+const productPrices = document.getElementsByClassName("price-buy");
+const products = [];
+
 clearCartElement.addEventListener("click", clearCart);
 
 for (let i = 0; i < cart.length; i++) {
   const element = cart[i];
-  element.addEventListener("click", addToCart);
+  element.addEventListener("click", function () {
+    const image = productImages[i].firstElementChild.firstElementChild;
+    const title = productTitles[i].firstElementChild.innerHTML;
+    const price = productPrices[i].firstElementChild.innerHTML;
+    const singleProduct = [image, title, price];
+    products.push(singleProduct);
+    
+    addToCart();
+  });
+  
 }
 
 function initTotalCart() {
   if (localStorage.cartClicks) {
     totalCart.innerHTML = localStorage.cartClicks;
-  } 
+  }
 }
 
 initTotalCart();
@@ -20,13 +34,18 @@ initTotalCart();
 function addToCart() {
   if (localStorage.cartClicks) {
       localStorage.cartClicks = Number(localStorage.cartClicks) + 1;
+      localStorage.setItem("products", products);
     } else {
       localStorage.cartClicks = 1;
     }
     totalCart.innerHTML = localStorage.cartClicks;
+
+    
 }
 
 function clearCart() {
   localStorage.cartClicks -= Number(localStorage.cartClicks);
   totalCart.innerHTML = localStorage.cartClicks;
+  products.length = 0;
+  localStorage.setItem("products", products);
 }
---
+++ scripts/total-cart.js
@@ -0,0 +1,15 @@
+function kurec() {
+    // const cars = [];
+    // let node = document.getElementById("price").cloneNode(true);
+    // cars.push(node);
+    // cars.push("title");
+    // cars.push("price");
+    // let imgTag = document.createElement("img");
+    // imgTag.setAttribute("src", "images/cross.png");
+    // cars.push(imgTag);
+    // //all that works
+    // for (let i = 0; i < cars.length; i++) {
+    //     console.log(cars[i]);
+    // }
+    console.log(localStorage.getItem("products"));
+}
